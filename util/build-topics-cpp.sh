a9bbb8099c49d8c6e63a01f6d38ae18e86d7414c
Andre Kutzleb <mail@andre-kutzleb.de>
2015-11-04 16:49:50 +0100

    Updated all topics and proto builder scripts

--- util/build-topics-cpp.sh
+++ util/build-topics-cpp.sh
@@ -8,8 +8,6 @@ OUT_DIR="${ZDIR}/common/cpp/zsdn-commons/zsdn/topics"
 echo "Topics Source Dir: ${SRC_DIR}"
 echo "Topics Out Dir: ${OUT_DIR}"
 
-cd hierarchy-builder/hierarchy-builder/target/
-
-java -jar ./hierarchy-builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar --source ${SRC_DIR} -r --out ${OUT_DIR} --language cpp -c "zsdn::modules"
+java -jar ./hierarchy-builder/hierarchy-builder/target/hierarchy-builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar --source ${SRC_DIR} -r --out ${OUT_DIR} --language cpp -c "zsdn::modules"
 
 echo "### Finished Building C++ Topics ###"
--- util/build-topics-java.sh
+++ util/build-topics-java.sh
@@ -8,8 +8,6 @@ OUT_DIR="${ZDIR}/common/java/zsdn-proto/src/main/java/zsdn/topics/"
 echo "Topics Source Dir: ${SRC_DIR}"
 echo "Topics Out Dir: ${OUT_DIR}"
 
-cd hierarchy-builder/hierarchy-builder/target/
-
-java -jar ./hierarchy-builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar --source ${SRC_DIR} -r --out ${OUT_DIR} --language java --java_package "zsdn.topics"
+java -jar ./hierarchy-builder/hierarchy-builder/target/hierarchy-builder-0.0.1-SNAPSHOT-jar-with-dependencies.jar --source ${SRC_DIR} -r --out ${OUT_DIR} --language java --java_package "zsdn.topics"
 
 echo "### Finished Building Java Topics ###"
