f7985bc33f29ed006fcc7dd87d35706355a96ab7
Rainer <st140590@stud.uni-stuttgart.de>
2015-11-04 19:14:14 +0100

    Overwrite old files, there are newer ones!

--- webgui/js/logging-angular.js
+++ webgui/js/logging-angular.js
@@ -2,36 +2,48 @@ var app = angular.module('zsdn-logging', [ 'smart-table' ]);
 
 app.controller('mainCtrl', [ '$scope','$http', function(scope, http) {
 
+	// store for data
 	scope.rowCollection = [];
-	
+	// store for displayed data 
 	scope.displayedCollection = [].concat(scope.rowCollection);
+	// request data from server
 	XHRUpdate();
 	
+	/**
+	 * Called when button update is clicked. \n
+	 * Request only new data from server.
+	 */
 	scope.update = function update() {
-		console.log("update call");
-		// TODO: Only load the data from last timestamp
-		console.log(new Date(scope.displayedCollection[5].timeStamp).getTime());
 		var startTime = new Date(scope.displayedCollection[0].timeStamp).getTime();
 		var endTime = new Date().getTime();
 		http.get(url + '/RESTAdmin/rest/messages/filtered?starttime=' + startTime + '&endtime=' + endTime).
-		then(function(response){
-			scope.rowCollection = scope.rowCollection.concat(response.data);
-			scope.displayedCollection = scope.displayedCollection.concat(scope.rowCollection);
-			console.log("new Data received");
-		}, function(response){
-			console.warn("Error while calling RESTAdmin/rest/messages. Status Code: " + response.status + " " + response.statusText);
+			then(function(response){
+				scope.rowCollection = scope.rowCollection.concat(response.data);
+				scope.displayedCollection = scope.displayedCollection.concat(scope.rowCollection);
+				//console.log("new Data received");
+			}, function(response){
+				// errorCallback
+				window.alert("Cassandra connect failed! Please verify that Cassandra is running and configured");
+				//window.alert(response.message);
+				console.warn("Error while calling RESTAdmin/rest/messages. Status Code: " + response.status + " " + response.statusText);
 		});
 	};
 	
+	/**
+	 * Sends XHR to server to get the data.
+	 */
 	function XHRUpdate() {
-		http.get(url + '/RESTAdmin/rest/messages' + '?' + Math.floor(Math.random() * 1000)).
-		then(function(response){
-			scope.rowCollection = response.data;
-			scope.displayedCollection = [].concat(scope.rowCollection);
-			console.log("new Data received");
-		}, function(response){
-			console.warn("Error while calling RESTAdmin/rest/messages. Status Code: " + response.status + " " + response.statusText);
+		http.get(url + '/RESTAdmin/rest/messages', {cache: false}).
+			then(function(response){
+				scope.rowCollection = response.data;
+				scope.displayedCollection = [].concat(scope.rowCollection);
+				//console.log("new Data received");
+			}, function(response){
+				// errorCallback
+				window.alert("Cassandra connect failed! Please verify that Cassandra is running and configured");
+				//window.alert(response.message);
+				console.warn("Error while calling RESTAdmin/rest/messages. Status Code: " + response.status + " " + response.statusText);
 		});
-	}
+	};
 	
-} ]);
+}]);
--- webgui/js/modules-angular.js
+++ webgui/js/modules-angular.js
@@ -1,54 +1,78 @@
 var app = angular.module('zsdn-modules', [ 'smart-table' ]);
 
 app.controller('moduleCtrl', [ '$scope','$http', function(scope, http) {
-
+	// store for data
 	scope.rowCollection = [];
-	
+	// store for displayed data 
 	scope.displayedCollection = [].concat(scope.rowCollection);
+	//request data from server
 	XHRUpdate();
 
+	/**
+	 * Called when button update is clicked.
+	 */
 	scope.update = function update() {
 		XHRUpdate();
 	};
 	
+	/**
+	 * Sends XHR to server to get the data.
+	 */
 	function XHRUpdate() {
-		http.get(url + '/RESTAdmin/rest/modules' + '?' + Math.floor(Math.random() * 1000)).
-		then(function(response){
-			scope.rowCollection = response.data;
-			scope.displayedCollection = [].concat(scope.rowCollection);
-			console.log("new Data received");
-		}, function(response){
-			console.warn("Error while calling RESTAdmin/rest/modules. Status Code: " + response.status + " " + response.statusText);
+		http.get(url + '/RESTAdmin/rest/modules',{cache: false}).
+			then(function (response){
+				scope.rowCollection = response.data;
+				scope.displayedCollection = [].concat(scope.rowCollection);
+				//console.log("new Data received");
+			}, function(response){
+				// errorCallback
+				//window.alert(response.message);
+				console.warn("Error while calling RESTAdmin/rest/modules. Status Code: " + response.status + " " + response.statusText);
 		});
-	}
+	};
 	
+	/**
+	 * Enable or disable a running module.
+	 */
 	scope.changeState = function changeState(row) {
 		if(row.currentState == "Active") {
 			// disable
 			http.put(url + '/RESTAdmin/rest/modules/control?id='+ row.UniqueId + '&action=disable').
-			then(function(response){
-				XHRUpdate();
-			}, function(response){
-				console.warn("Error during disable! --> Status Code: " + response.status + " " + response.statusText);
+				then(function(response){
+					XHRUpdate();
+				}, function(response){
+					// errorCallback
+					//window.alert(response.message);
+					console.warn("Error during disable! --> Status Code: " + response.status + " " + response.statusText);
+					XHRUpdate();
 			});
 		} else if (row.currentState == "Inactive") {
 			// enable
 			http.put(url + '/RESTAdmin/rest/modules/control?id='+ row.UniqueId + '&action=enable').
+				then(function(response){
+					XHRUpdate();
+				}, function(response){
+					// errorCallback
+					//window.alert(response.message);
+					console.warn("Error during enable! --> Status Code: " + response.status + " " + response.statusText);
+					XHRUpdate();
+			});
+		}
+	};
+	
+	/**
+	 * Stop a running module.
+	 */
+	scope.stopModule = function stopModule(row) { 
+		http.put(url + '/RESTAdmin/rest/modules/control?id='+ row.UniqueId + '&action=stop').
 			then(function(response){
 				XHRUpdate();
 			}, function(response){
-				console.warn("Error during enable! --> Status Code: " + response.status + " " + response.statusText);
-			});
-		}
-	}
-	
-	scope.stopModule = function stopModule(row) { 
-		http.put(url + '/RESTAdmin/rest/modules/control?id='+ row.UniqueId + '&action=stop').
-		then(function(response){
-			XHRUpdate();
-		}, function(response){
-			console.warn("Error during stop! --> Status Code: " + response.status + " " + response.statusText);
+				// errorCallback
+				//window.alert(response.message);
+				console.warn("Error during stop! --> Status Code: " + response.status + " " + response.statusText);
+				XHRUpdate();
 		});
-	}
+	};
 	
-} ]);
+}]);
