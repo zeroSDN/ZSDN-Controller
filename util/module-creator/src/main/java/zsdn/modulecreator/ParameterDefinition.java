04f2152530a479ea5a84efc91234709064c093f4
Andre Kutzleb <mail@andre-kutzleb.de>
2015-11-09 03:15:49 +0100

    added java template module and updated module creator

--- util/module-creator/src/main/java/zsdn/modulecreator/ParameterDefinition.java
+++ util/module-creator/src/main/java/zsdn/modulecreator/ParameterDefinition.java
@@ -10,7 +10,7 @@ public class ParameterDefinition {
 	@Parameter(names = {"--template_folder", "-t"}, description = "folder of template module.", required = true)
 	protected String templateFolder;
 	
-	@Parameter(names = {"--template_ut_folder","-u"}, description = "folder of template module unittest.",required = true)
+	@Parameter(names = {"--template_ut_folder","-u"}, description = "folder of template module unittest.")
 	protected String templateUnitestFolder;
 	
 	@Parameter(names = {"--target_folder", "-d"}, description = "folder where module should be created.",required = true)
