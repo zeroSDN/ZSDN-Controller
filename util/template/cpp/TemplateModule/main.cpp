3177a05f1719a285117810d5e27a66038976469f
Andre Kutzleb <mail@andre-kutzleb.de>
2015-11-10 14:03:48 +0100

    updated main.cpp of template module

--- util/template/cpp/TemplateModule/main.cpp
+++ util/template/cpp/TemplateModule/main.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
 
 #include <memory>
-#include <Poco/Exception.h>
 #include <zmf/AbstractModule.hpp>
-#include <zmf/ZmfConsole.hpp>
-#include "zmf/ZmfInstance.hpp"
 #include "TemplateModule.hpp"
+#include "zsdn/StartupHelper.h"
 
 
-int main() {
+int main(int argc, char* argv[]) {
 
-    std::cout << "Starting TemplateModule. Please enter instance ID: ";
-    uint64_t instanceId;
-    std::cin >> instanceId;
+	// Default instance id = 0, if multiple modules of same type should run in paralell, 
+	// each one has to start with a unique one.
+	uint64_t instanceId = 0;
 
-    std::vector<zmf::instance::ZmfInstance::StartOption> startOptions = {
-            // zmf::instance::ZmfInstance::StartOption::NO_AUTO_ENABLE, // module won't be enabled automatically. If left out, module will be enabled as soon as possible (dependencies satisfied)
-            //zmf::instance::ZmfInstance::StartOption::NO_EXIT_WHEN_ENABLE_FAILED, // No shutdown when module enabling failed
-            //zmf::instance::ZmfInstance::StartOption::NO_PEER_DISCOVERY_WAIT // if left out, starting module will wait until all peers are discovered
+	std::vector<zmf::instance::ZmfInstance::StartOption> startOptions = {
+            // zmf::instance::ZmfInstance::StartOption::NO_AUTO_ENABLE, 		// module won't be enabled automatically. If not used, module will be enabled as soon as possible (dependencies satisfied)
+            // zmf::instance::ZmfInstance::StartOption::NO_EXIT_WHEN_ENABLE_FAILED, 	// If not used, module will simply remain disabled instead of shutting down when enable fails.
+            // zmf::instance::ZmfInstance::StartOption::NO_PEER_DISCOVERY_WAIT 		// if not used, modules will start quicker, but may not know about all other modules when enabled yet.
+	    // zmf::instance::ZmfInstance::StartOption::NO_EQUAL_MODULE_INTERCONNECT 	// if not used, modules of the same type will connect to each other. for modules that have many instances, this causes a lot of unecessary interconnections (unless they are actually needed).
     };
 
-    try {
-        // Create module
-        std::shared_ptr<zmf::AbstractModule> module = std::shared_ptr<zmf::AbstractModule>(
-                new TemplateModule(instanceId));
-        // Create and start ZMF instance with module
-        std::shared_ptr<zmf::IZmfInstanceController> zmfInstance = zmf::instance::ZmfInstance::startInstance(module,
-                                                                                                    startOptions);
-        // Start console
-        zmf::ZmfConsole console(zmfInstance);
-        console.startConsole();
+   	int returnCode;
+    if(zsdn::StartupHelper::paramsOkay(argc,argv)) {
+    	// Load the logger configuration before anyone instanciates a logger
+        zmf::logging::ZmfLogging::initializeLogging("TemplateModule", argv[1]); 
+        // Start the module
+        returnCode = zsdn::StartupHelper::startInConsole(new TemplateModule(instanceId), argv[1],startOptions);
+    } else {
+        returnCode = 1;
     }
-    catch (Poco::Exception exc) { // Catch Exceptions
-        std::cerr << "Failed to run module: " << exc.message() << &std::endl;
-        return 1;
-    }
-    catch (...) {  // Catch all
-        std::cerr << "Failed to run module: Unknown reason" << &std::endl;
-        return 1;
-    }
-
-    return 0;
+    return returnCode;
 }
