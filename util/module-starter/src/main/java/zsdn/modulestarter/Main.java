4329bbcb4e342cbec7c2b31a560e4a4a085c78c1
Jonas Grunert <git@jonas-grunert.de>
2015-11-11 00:32:04 -0800

    Added modulestarter

---
+++ util/module-starter/src/main/java/zsdn/modulestarter/Main.java
@@ -0,0 +1,26 @@
+package zsdn.modulestarter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.ParameterException;
+
+/**
+ *
+ * 
+ * @author Jonas Grunert
+ *
+ */
+public class Main {
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+	}
+
+}
