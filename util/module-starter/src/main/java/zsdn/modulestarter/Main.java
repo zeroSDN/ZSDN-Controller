d4666b2ebfb0e5239aa3cb37d29a0675ff51b518
Jonas Grunert <git@jonas-grunert.de>
2015-11-11 01:49:11 -0800

    Module selector starting modules from parameters

--- util/module-starter/src/main/java/zsdn/modulestarter/Main.java
+++ util/module-starter/src/main/java/zsdn/modulestarter/Main.java
@@ -1,16 +1,10 @@
 package zsdn.modulestarter;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.apache.commons.io.FileUtils;
-
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.ParameterException;
 
 /**
  *
@@ -21,6 +15,28 @@ import com.beust.jcommander.ParameterException;
 public class Main {
 	public static void main(String[] args) throws IOException, InterruptedException {
 
-	}
+		Pattern pattern = Pattern.compile("\\s*((\".*?\")|\\S+)\\s*");
 
+		List<Process> started = new ArrayList<>();
+
+		for (String line : args) {
+			Matcher matcher = pattern.matcher(line);
+
+			List<String> commands = new ArrayList<>();
+			while (matcher.find()) {
+				commands.add(matcher.group().trim());
+			}
+			ProcessBuilder b = new ProcessBuilder();
+			b.inheritIO();
+			b.command(commands);
+			started.add(b.start());
+		}
+
+		final Thread shutdownThread = new Thread(() -> started.forEach(Process::destroy));
+		Runtime.getRuntime().addShutdownHook(shutdownThread);
+
+		for (Process p : started) {
+			p.waitFor();
+		}
+	}
 }
